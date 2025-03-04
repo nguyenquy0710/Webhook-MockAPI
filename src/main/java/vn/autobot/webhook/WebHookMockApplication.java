7c70feb633755cd14b94ce0759e25e94275773a3
ChuyenNS <chuyenns@fpt.com>
2025-03-04 12:35:15 +0700

    update port and config timezone default

--- src/main/java/vn/autobot/webhook/WebHookMockApplication.java
+++ src/main/java/vn/autobot/webhook/WebHookMockApplication.java
@@ -3,8 +3,16 @@ package vn.autobot.webhook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class WebHookMockApplication {
+
+    static {
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", "9");
+        System.setProperty("java.util.concurrent.ForkJoinPool.common.threadFactory", "9");
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebHookMockApplication.class, args);
